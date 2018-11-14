f15c39aeb21a320e93174c1f6cebc9ff07568036
Ainhoa Valle <a.ainhoavalle@gmail.com>
2018-11-14 12:00:35 +0100

    Componente temporizador con Interface para crear evento al finalizar la cuenta atrás.

--- Componentes/src/BotonPropio/BotonEjercicio.java
+++ Componentes/src/BotonPropio/BotonEjercicio.java
@@ -1,4 +1,4 @@
-package Modelo;
+package BotonPropio;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
---
+++ Componentes/src/Temporizador/CuentaAtrasFinalizadaListener.java
@@ -0,0 +1,10 @@
+package Temporizador;
+
+/**
+ *
+ * @author Ainhoa
+ */
+public interface CuentaAtrasFinalizadaListener {
+    
+    public void ejecutar();
+}
